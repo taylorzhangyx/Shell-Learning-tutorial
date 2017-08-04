a9ccad80d0a198a1194408aa35ba89bc6bf3059a
Yuxin Zhang <taylorzhangyx@gmail.com>
2017-08-03 17:41:10 -0700

    variable tutorial

--- my-script.sh
+++ my-script.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-Hello World
+# This is a comment!
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
